52743abdb38fb39810d9223ca61b38adc6067a92
Dan Dart <git@dandart.co.uk>
2025-06-08 22:36:04 +0100

    bulidables

--- shell.nix
+++ shell.nix
@@ -5,9 +5,6 @@ mkShell rec {
         haskell.compiler.ghc912
         cabal-install
         krank
-        pkg-config
-        zlib.dev
-        pcre.dev
         gettext
         nodejs
         php84
@@ -17,6 +14,6 @@ mkShell rec {
         [[ -f ~/.local/bin/refactor ]] || cabal install apply-refact cabal-fmt doctest ghci-dap ghcid ghcide haskell-debug-adapter haskell-language-server hasktags hlint hoogle hpack implicit-hie stan stylish-haskell weeder --overwrite-policy=always --allow-newer
         export PATH=~/.local/bin:$PATH
         gen-hie > hie.yaml
-        for i in $(find -type f | grep -v dist-newstyle); do krank $i; done
+        for i in $(find -type f | grep -v "dist-*"); do krank $i; done
     '';
 }
