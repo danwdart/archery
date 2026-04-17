5daa07209ba3d23d81f72f5225ae86930bde7849
Dan Dart <git@dandart.co.uk>
2026-04-17 21:32:43 +0000

    fooling

--- lib/Data/Function/Free/Abstract.hs
+++ lib/Data/Function/Free/Abstract.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE OverloadedLists       #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuantifiedConstraints #-}
@@ -18,6 +19,7 @@ import Control.Category.Numeric
 import Control.Category.Strong
 import Control.Category.Symmetric
 import Data.Aeson
+import Data.Aeson.Types
 import Prelude                      hiding (id, (.))
 import Control.Arrow
 
@@ -115,17 +117,85 @@ instance (forall a b. ToJSON (k a b)) ⇒ ToJSON (FreeFunc k x y) where
     toJSON ReassocEither = String "ReassocEither"
     toJSON (Lift f) = Array [ "Lift", Array [ toJSON f ] ]
 
-instance FromJSON (FreeFunc p a a) where
-    parseJSON (String "Id") = pure Id
-    parseJSON a             = fail $ "TypeError: got " <> show a <> ", expecting a -> a"
+-- be specific here for now
+instance FromJSON (FreeFunc k (Int, Int) Int) where
+    parseJSON (String "Add") = pure Add
+    parseJSON (String "Mult") = pure Mult
+    parseJSON (String "Div") = pure Div
+    parseJSON (String "Mod") = pure Mod
+    parseJSON a = typeMismatch "(Int, Int) -> Int" a
 
--- instance (FromJSON (FreeFunc p a b), FromJSON (FreeFunc p b c)) => FromJSON (FreeFunc p a c) where
---     parseJSON [String "Compose", Array [a, b]] = pure $ Compose (parseJSON a) (parseJSON b)
+instance FromJSON (FreeFunc k Int Int) where
+    parseJSON (String "Negate") = pure Negate
+    parseJSON a = typeMismatch "Int -> Int" a
+
+instance FromJSON (FreeFunc k (Integer, Integer) Integer) where
+    parseJSON (String "Add") = pure Add
+    parseJSON (String "Mult") = pure Mult
+    parseJSON (String "Div") = pure Div
+    parseJSON (String "Mod") = pure Mod
+    parseJSON a = typeMismatch "(Integer, Integer) -> Integer" a
+
+instance FromJSON (FreeFunc k Integer Integer) where
+    parseJSON (String "Negate") = pure Negate
+    parseJSON a = typeMismatch "Integer -> Integer" a
+
+instance FromJSON (FreeFunc k (Float, Float) Float) where
+    parseJSON (String "Add") = pure Add
+    parseJSON (String "Mult") = pure Mult
+    parseJSON a = typeMismatch "(Float, Float) -> Float" a
+
+instance FromJSON (FreeFunc k Float Float) where
+    parseJSON (String "Negate") = pure Negate
+    parseJSON a = typeMismatch "Integer -> Integer" a
+
+instance FromJSON (FreeFunc k (Double, Double) Double) where
+    parseJSON (String "Add") = pure Add
+    parseJSON (String "Mult") = pure Mult
+    parseJSON a = typeMismatch "(Double, Double) -> Double" a
+
+instance FromJSON (FreeFunc k Double Double) where
+    parseJSON (String "Negate") = pure Negate
+    parseJSON a = typeMismatch "Integer -> Integer" a
+
+    {-
+    FreeFunc p x x
+    Compose :: FreeFunc p y z -> FreeFunc p x y -> FreeFunc p x z
+    Copy :: FreeFunc p x (x, x)
+    Consume :: FreeFunc p x ()
+    First :: FreeFunc p a b -> FreeFunc p (a, x) (b, x)
+    Second :: FreeFunc p a b -> FreeFunc p (x, a) (x, b)
+    {- Unfirst :: FreeFunc p (a, x) (b, x) -> FreeFunc p a b -}
+    Fst :: FreeFunc p (a, b) a
+    Snd :: FreeFunc p (a, b) b
+    InjectL :: FreeFunc p a (Either a b)
+    InjectR :: FreeFunc p a (Either b a)
+    Left' :: FreeFunc p a b -> FreeFunc p (Either a x) (Either b x)
+    Right' :: FreeFunc p a b -> FreeFunc p (Either x a) (Either x b)
+    {- Unleft :: FreeFunc p (Either a x) (Either b x) -> FreeFunc p a b -}
+    Unify :: FreeFunc p (Either a a) a
+    Tag :: FreeFunc p (Bool, a) (Either a a)
+    Num :: (Num n, Show n, ToJSON n) => n -> FreeFunc p a n
+    Swap :: FreeFunc p (a, b) (b, a)
+    SwapEither :: FreeFunc p (Either a b) (Either b a)
+    Reassoc :: FreeFunc p (a, (b, c)) ((a, b), c)
+    ReassocEither :: FreeFunc p (Either a (Either b c)) (Either (Either a b) c)
+    Lift :: p a b -> FreeFunc p a b
+    -}
+
+-- instance {-# OVERLAPPING #-} (forall a. ToJSON (k a a), x ~ y) ⇒ FromJSON (FreeFunc k x x) where
+--     parseJSON (String "Id") = pure Id
+--     parseJSON a             = fail $ "TypeError: got " <> show a <> ", expecting a -> a"
+-- 
+-- -- (forall a b. FromJSON (FreeFunc k a b), forall b c. FromJSON (FreeFunc k b c)) => 
+-- instance {-# OVERLAPPING #-} FromJSON (FreeFunc k x z) where
+--     parseJSON (Array [String "Compose", Array [a, b]]) = Compose <$> parseJSON a <*> parseJSON b
 --     parseJSON a             = fail $  "TypeError: got " <> show a <> ", expecting (b -> c) -> (a -> b) -> a -> c"
-
-instance FromJSON (FreeFunc p a (a, a)) where
-    parseJSON (String "Copy") = pure Copy
-    parseJSON a = fail $ "TypeError: got " <> show a <> ", expecting a -> (a, a)"
+-- 
+-- -- (forall a. FromJSON (k a (a, a))) ⇒ 
+-- instance {-# OVERLAPPING #-} FromJSON (FreeFunc k x (x, x)) where
+--     parseJSON (String "Copy") = pure Copy
+--     parseJSON a = fail $ "TypeError: got " <> show a <> ", expecting a -> (a, a)"
 
 -- instance (FromJSON (p a b)) => FromJSON (FreeFunc p a b) where
 --     parseJSON (Array [ String "Lift", x ] ) = pure $ Lift (parseJSON x)
